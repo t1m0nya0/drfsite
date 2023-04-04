d38610f183ba3384ad2027df484029517e2d9692
TamirlanNurbulatuly <t.nurbulatuly@gmail.com>
2023-04-04 23:09:04 +0600

    CRUD by 1 function and add router class

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework import routers
 from women.views import *
 
+router = routers.SimpleRouter()
+router.register(r'women', WomenViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomenAPIList.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
-    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
+    path('api/v1/', include(router.urls)),
 ]
